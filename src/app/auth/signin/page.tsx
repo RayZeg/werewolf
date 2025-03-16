17887d5d48f14df4cee88a31dcf6f4301f1f44e0
RayZeg <dzgamingmaster@gmail.com>
2025-03-16 02:42:56 +0100

    cleanup + added game to db

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Eye, EyeOff, RotateCw } from "lucide-react";
 import { useActionState, useState } from "react";
-import { signin } from "../actions";
+import { signin } from "../auth-action";
 
 interface User {
   username: string;
--- src/components/UserIcon.tsx
+++ src/components/UserIcon.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signout } from "@/app/auth/actions";
+import { signout } from "@/app/auth/auth-action";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,16 +9,21 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { getSession } from "@/lib/session";
 import { LogOut, User } from "lucide-react";
 
-export default function UserIcon() {
+export default function UserIcon({
+  session,
+}: {
+  session: { userId: string; username: string };
+}) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="absolute top-0 right-0 m-2 bg-white rounded-full p-1 border-2 border-gray-200">
         <User />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuLabel>{session.username}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>Profile</DropdownMenuItem>
         <DropdownMenuItem className="text-destructive" onClick={signout}>
